cb21a2ef33798b36975581df084bd9c4ee57ccf1
unknown <diana.chorna@gmail.com>
2021-06-01 19:23:50 +0300

    a

---
+++ main.js
@@ -0,0 +1,8 @@
+let sel = x =>document.querySelector(x);
+
+$(function(){
+      $('.red').slideDown(1500, function(){
+            $('.red').slideUp(1500,)
+            sel('.number').textContent= '1';
+      })
+})
